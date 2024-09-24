2df66264f2c9bd36e71c767d429e189a84b2c11d
Douglas Pimentel <dev.pimentel@gmail.com>
2024-09-24 10:44:18 -0300

    api: teste

--- apps/api/supabase/tests/database/schema.test.sql
+++ apps/api/supabase/tests/database/schema.test.sql
@@ -1,9 +1,18 @@
 BEGIN;
-SELECT plan( 2 );
+SELECT plan( 10 );
 
 SELECT has_schema( 'public', 'public schema should exist' );
-
 SELECT has_schema( 'auth', 'auth schema should exist' );
+
+SELECT has_table( 'public', 'users', 'users table should exist' );
+SELECT has_column( 'public', 'users', 'id', 'id should exist' );
+SELECT col_is_pk ( 'public', 'users', 'id', 'id is a primary key' );
+SELECT has_column( 'public', 'users', 'first_name', 'first_name should exist' );
+SELECT has_column( 'public', 'users', 'last_name', 'last_name should exist' );
+SELECT has_column( 'public', 'users', 'role', 'role should exist' );
+SELECT has_column( 'public', 'users', 'created_at', 'created_at should exist' );
+SELECT has_column( 'public', 'users', 'updated_at', 'updated_at should exist' );
+
 
 SELECT * FROM finish();
 ROLLBACK;
